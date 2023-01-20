c9c96fcc5a90569b10d7c9403df34baeaa6dd05a
ardatoprak <ardattoprak@gmail.com>
2023-01-20 23:50:28 +0300

    2 day picker added and its functions wrote

--- Hotel/Controllers/AddRegistrationTableViewController.swift
+++ Hotel/Controllers/AddRegistrationTableViewController.swift
@@ -14,25 +14,63 @@ class AddRegistrationTableViewController: UITableViewController {
     @IBOutlet var lastNameTextField: UITextField!
     @IBOutlet var emailTextField: UITextField!
     
+    @IBOutlet var checkInDatelabel:UILabel!
+    @IBOutlet var checkInDatePicker: UIDatePicker!
+    @IBOutlet var checkOutDatelabel:UILabel!
+    @IBOutlet var checkOutDatePicker: UIDatePicker!
+    
     // MARK: - Properties
     
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        // CheckIn tarihini, bugünün gecesi olarak ayarlar.
+        let midnightToday = Calendar.current.startOfDay(for: Date())
+        
+        checkInDatePicker.minimumDate = midnightToday
+        checkInDatePicker.date = midnightToday
+
     }
     
     // MARK: - Functions
+    func updateDateViews() {
+        
+        // 21/03/1996 --> .short --> 3:30 PM
+        // 21/03/1996 --> .long --> March 21, 1996
+        
+        // date üzerine belirtilen miktarda saniye ekler.
+        let oneDay: Double = 24 * 60 * 60
+        checkOutDatePicker.minimumDate = checkInDatePicker.date.addingTimeInterval(oneDay)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        
+        checkInDatelabel.text = dateFormatter.string(from: checkInDatePicker.date)
+        checkOutDatelabel.text = dateFormatter.string(from: checkOutDatePicker.date)
+        
+        
+    }
     
     // MARK: - Actions
     @IBAction func doneBarButtonTapped(_ button: UIBarButtonItem){
         let firstname = firstNameTextField.text!
         let lastname = lastNameTextField.text!
         let email = emailTextField.text!
+        let checkInDate = checkInDatePicker.date
+        let checkOutDate = checkOutDatePicker.date
         
         print("Done Tapped")
         print("Firstname: \(firstname)")
         print("Lastname: \(lastname)")
         print("Email: \(email)")
+        print("CheckIn: \(checkInDate)")
+        print("CheckOut: \(checkOutDate)")
+    }
+    
+    @IBAction func datePickerValueChanged(_ picker: UIDatePicker){
+        updateDateViews()
     }
     
 }
