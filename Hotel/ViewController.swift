314e77a95ce0029400f44a9836b8a170bd8f6b2d
ardatoprak <ardattoprak@gmail.com>
2023-01-20 22:01:06 +0300

    Initial Commit

---
+++ Hotel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hotel
+//
+//  Created by Arda Toprak on 20.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
